b0ec274278cac4d70ee2beec59505f3c751dc9bd
Arthur Melin <arthur.melin@epitech.eu>
2016-12-27 23:46:40 +0100

    Fix memcmp

--- src/my_memcmp.c
+++ src/my_memcmp.c
@@ -5,19 +5,17 @@
 ** Login   <arthur.melin@epitech.eu>
 **
 ** Started on  Sat Dec 24 00:54:01 2016 Arthur Melin
-** Last update Sat Dec 24 00:59:56 2016 Arthur Melin
+** Last update Tue Dec 27 23:46:16 2016 Arthur Melin
 */
 
 #include "my.h"
 
 int	my_memcmp(void *s1, void *s2, int n)
 {
-  if (n == 0)
-    return (0);
-  while (n-- && *(unsigned char *)s1 == *(unsigned char *)s2)
-    {
-      s1++;
-      s2++;
-    }
-  return (*(unsigned char *)s1 - *(unsigned char *)s2);
+  int	v;
+
+  v = 0;
+  while (n-- && v == 0)
+    v = *(unsigned char *)s1 - *(unsigned char *)s2;
+  return (v);
 }
