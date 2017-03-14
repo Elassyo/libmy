bb87fa96d5b5eced9024fbbc33f7748419bec4f4
Arthur Melin <arthur.melin@epitech.eu>
2017-03-14 22:23:24 +0100

    Consistency? Meh.

--- src/my_issign.c
+++ src/my_issign.c
@@ -5,8 +5,10 @@
 ** Login   <arthur.melin@epitech.eu>
 **
 ** Started on  Mon Nov  7 15:41:44 2016 Arthur Melin
-** Last update Mon Nov  7 15:41:49 2016 Arthur Melin
+** Last update Tue Mar 14 22:22:38 2017 Arthur Melin
 */
+
+#include <my.h>
 
 int	my_issign(char c)
 {
--- src/my_power_ite.c
+++ src/my_power_ite.c
@@ -5,8 +5,10 @@
 ** Login   <arthur.melin@epitech.eu>
 **
 ** Started on  Wed Nov 16 21:49:14 2016 Arthur Melin
-** Last update Wed Nov 16 21:52:49 2016 Arthur Melin
+** Last update Tue Mar 14 22:22:58 2017 Arthur Melin
 */
+
+#include <my.h>
 
 long	my_power_ite(int base, int power)
 {
