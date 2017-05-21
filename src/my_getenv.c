94994db7a55f27a78b76dfa74e909abfdc9ea9b6
Arthur Melin <arthur.melin@epitech.eu>
2017-05-21 19:38:06 +0200

    .

---
+++ src/my_getenv.c
@@ -0,0 +1,29 @@
+/*
+** my_getenv.c for libmy in /home/arthur.melin/Code/libmy
+**
+** Made by Arthur Melin
+** Login   <arthur.melin@epitech.eu>
+**
+** Started on  Sun May 21 19:28:45 2017 Arthur Melin
+** Last update Sun May 21 19:37:01 2017 Arthur Melin
+*/
+
+#include <stdlib.h>
+#include <my.h>
+
+extern char	**environ;
+
+char		*my_getenv(const char *name)
+{
+  int		i;
+  size_t	len;
+
+  i = 0;
+  len = my_strlen(name);
+  while (environ && environ[i])
+    {
+      if (my_strncmp(environ[i], name, len) == 0 && environ[i][len] == '=')
+	return (environ[i] + len + 1);
+    }
+  return (NULL);
+}
